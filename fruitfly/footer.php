7ded4acead8d2f025aa3b8c91b010b8f586e0afa
sylvia <67394526+axlevxa@users.noreply.github.com>
2021-11-28 16:42:48 -0800

    improved css

--- fruitfly/footer.php
+++ fruitfly/footer.php
@@ -20,13 +20,7 @@ $text_color = get_field('text_color');
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'fruitfly' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fruitfly' ), 'fruitfly', '<a href="https://thelonghaul.org">long haul collective</a>' );
-				?>
+				?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
